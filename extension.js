2c98614b35bd9face96550dd05ac851d32c8d419
ArturoDent <31813701+ArturoDent@users.noreply.github.com>
2022-01-05 10:09:47 -0700

    - initial commit

---
+++ extension.js
@@ -0,0 +1,31 @@
+const vscode = require('vscode');
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+
+	let disposable = vscode.commands.registerCommand('convert-selection.surround', function () {
+
+    const editor = vscode.window.activeTextEditor;
+    const selections = editor.selections;
+    const newSelections = [];
+    
+    for (const selection of selections) {
+      const newSelection1 = new vscode.Selection(selection.start, selection.start);
+      const newSelection2 = new vscode.Selection(selection.end, selection.end);
+      newSelections.push(newSelection1, newSelection2);
+    }
+
+	  editor.selections = newSelections;
+    
+    
+    // vscode.window.showInformationMessage('There are no selections in the document');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+exports.activate = activate;
